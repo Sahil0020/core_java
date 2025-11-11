009220c7cf3a8527d78decd5776d9525e1e54833
Sahil020 <sahiljoshi29632@gmail.com>
2025-11-11 22:12:53 +0530

    Multilevel Inheritence

---
+++ src/day10/Puppy.java
@@ -0,0 +1,15 @@
+package day10;
+
+public class Puppy extends Dog {
+	public Puppy()
+	{
+		System.out.println("Puppy object created");
+		
+	}
+	public void weep()
+	{
+		System.out.println("puppy is weeping");
+	}
+	
+
+}
